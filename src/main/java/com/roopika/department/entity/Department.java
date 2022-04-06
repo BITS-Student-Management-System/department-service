56ec6f1bcb8b3fa8a14d9cc81e27c83f88cb389c
Roopika Srinivas <13376391+roopikasrinivas@users.noreply.github.com>
2022-04-06 20:57:33 +0530

    Model Changes

--- src/main/java/com/roopika/department/entity/Department.java
+++ src/main/java/com/roopika/department/entity/Department.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -17,8 +14,9 @@ public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
     private Long departmentId;
-    private String departmentName;
-    private String departmentAddress;
     private String departmentCode;
+    private String departmentName;
+    private int numberOfStudents;
 }
